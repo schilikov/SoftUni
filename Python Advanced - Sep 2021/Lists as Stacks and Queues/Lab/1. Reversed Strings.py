345023d7494ae8bf534bb9e2bacb844e64c5a77b
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-14 22:40:33 +0200

    Added Lab/2. Matching Parentheses.py

--- Python Advanced - Sep 2021/Lists as Stacks and Queues/Lab/1. Reversed Strings.py
+++
@@ -1,8 +0,0 @@
-# We must reverse and move the given string from "word" to "result"
-word = list(input())
-result = []
-
-while len(word) > 0:
-    result.append(word.pop())
-
-print(f"{''.join(result)}")
