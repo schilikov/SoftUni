64fcfc79e57c68d6de569227167fd3aba8474d43
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-14 22:42:37 +0200

    Added exercise

---
+++ Python Advanced - Sep 2021/Lists as Stacks and Queues/Lab/1. Reverse Strings.py
@@ -0,0 +1,8 @@
+# We must reverse and move the given string from "word" to "result"
+word = list(input())
+result = []
+
+while len(word) > 0:
+    result.append(word.pop())
+
+print(f"{''.join(result)}")
