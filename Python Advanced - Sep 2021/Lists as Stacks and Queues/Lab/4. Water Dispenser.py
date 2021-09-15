4793d2ac7a33967c6d238d9babafaba081e0e5d3
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-16 00:41:41 +0200

    Added Lab/4. Water Dispenser.py

---
+++ Python Advanced - Sep 2021/Lists as Stacks and Queues/Lab/4. Water Dispenser.py
@@ -0,0 +1,27 @@
+# We should print who got water, who should wait and how many liters are left
+from collections import deque
+
+liters = int(input())
+name = input()
+queue = deque()
+
+while not name == "Start":
+    queue.append(name)
+    name = input()
+
+command = input()
+
+while not command == "End":
+    if command.startswith("refill"):
+        liters += int(command.split()[-1])
+    else:
+        liters_wanted = int(command)
+        name = queue.popleft()
+        if liters >= liters_wanted:
+            liters -= liters_wanted
+            print(f"{name} got water")
+        else:
+            print(f"{name} must wait")
+    command = input()
+
+print(f"{liters} liters left")
