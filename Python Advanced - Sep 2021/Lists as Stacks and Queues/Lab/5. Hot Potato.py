eb8fb55d394c83e15e3da30ac2a0c722cffaa6fa
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-16 01:08:08 +0200

    Added Lab/5. Hot Potato.py

---
+++ Python Advanced - Sep 2021/Lists as Stacks and Queues/Lab/5. Hot Potato.py
@@ -0,0 +1,11 @@
+# A game of hot potato
+from collections import deque
+
+children = deque(input().split())
+count = int(input())
+
+while len(children) > 1:
+    children.rotate(-count)
+    print(f"Removed {children.pop()}")
+
+print(f"Last is {children.popleft()}")
