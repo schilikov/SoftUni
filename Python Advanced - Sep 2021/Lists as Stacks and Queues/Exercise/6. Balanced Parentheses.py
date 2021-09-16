a4473907990cb394ceae691e645c1a2ca079b2e4
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-17 00:41:34 +0200

    Added Exercise/6. Balanced Parentheses.py

---
+++ Python Advanced - Sep 2021/Lists as Stacks and Queues/Exercise/6. Balanced Parentheses.py
@@ -0,0 +1,23 @@
+# Are the parentheses balanced or not?
+parentheses = input()
+stack = []
+balanced = True
+
+for ch in parentheses:
+    if ch in "[{(":
+        stack.append(ch)
+    elif ch in ")}]":
+        if len(stack) == 0:
+            balanced = False
+            break
+
+        opening_paren = stack.pop()
+
+        if f"{opening_paren}{ch}" not in ["[]", "{}", "()"]:
+            balanced = False
+            break
+
+if balanced and len(stack) == 0:
+    print("YES")
+else:
+    print("NO")
