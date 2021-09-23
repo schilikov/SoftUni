bbd3b0330979858fd3275db0e945e8301756c9dd
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-23 15:28:32 +0200

    Changed

--- Python Advanced - Sep 2021/Stacks, Queues, Tuples and Sets/Stacks, Queues, Tuples and Sets - Exercise/1. Numbers.py
+++ Python Advanced - Sep 2021/Stacks, Queues, Tuples and Sets/Stacks, Queues, Tuples and Sets - Exercise/1. Numbers.py
@@ -1,5 +1,23 @@
-bees = set([int(x) for x in input().split()])
-collected_nectar = set([int(x) for x in input().split()])
+# Use the commands to edit the sets and then print them
+first = set([int(x) for x in input().split()])
+second = set([int(x) for x in input().split()])
 
-print(bees)
-print(collected_nectar)
+n = int(input())
+for _ in range(n):
+    line_args = input().split()
+    command = line_args[0]
+    if command == "Add" and line_args[1] == "First":
+        [first.add(int(x)) for x in line_args[2:]]
+    elif command == "Add" and line_args[1] == "Second":
+        [second.add(int(x)) for x in line_args[2:]]
+    elif command == "Remove" and line_args[1] == "First":
+        current_set = set([int(x) for x in line_args[2:]])
+        first = first.difference(current_set)
+    elif command == "Remove" and line_args[1] == "Second":
+        current_set = set([int(x) for x in line_args[2:]])
+        second = second.difference(current_set)
+    else:
+        print(first.issubset(second) or second.issubset(first))
+
+print(", ".join([str(x) for x in sorted(first)]))
+print(", ".join([str(x) for x in sorted(second)]))
