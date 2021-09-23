cb77045231486db134b3e510426a5df01f296af3
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-23 15:26:23 +0200

    Changed

--- Python Advanced - Sep 2021/Stacks, Queues, Tuples and Sets/Stacks, Queues, Tuples and Sets - Exercise/1. Numbers.py
+++ Python Advanced - Sep 2021/Stacks, Queues, Tuples and Sets/Stacks, Queues, Tuples and Sets - Exercise/1. Numbers.py
@@ -1,41 +1,5 @@
-# Make milkshakes (or don`t)
-from collections import deque
+bees = set([int(x) for x in input().split()])
+collected_nectar = set([int(x) for x in input().split()])
 
-chocolates = [int(x) for x in input().split(", ")]
-cup_of_milk = deque([int(x) for x in input().split(", ")])
-
-milkshakes = 0
-
-while chocolates and cup_of_milk and milkshakes < 5:
-    chocolate = chocolates.pop()
-    milk_cup = cup_of_milk.popleft()
-
-    if chocolate <= 0 and milk_cup <= 0:
-        continue
-    if chocolate <= 0:
-        cup_of_milk.appendleft(milk_cup)
-        continue
-    if milk_cup <= 0:
-        chocolates.append(chocolate)
-        continue
-
-    if chocolate == milk_cup:
-        milkshakes += 1
-    else:
-        cup_of_milk.append(milk_cup)
-        chocolates.append(chocolate - 5)
-
-if milkshakes == 5:
-    print("Great! You made all the chocolate milkshakes needed!")
-else:
-    print("Not enough milkshakes.")
-
-if chocolates:
-    print(f"Chocolate: {', '.join([str(x) for x in chocolates])}")
-else:
-    print("Chocolate: empty")
-
-if cup_of_milk:
-    print(f"Milk: {', '.join([str(x) for x in cup_of_milk])}")
-else:
-    print("Milk: empty")
+print(bees)
+print(collected_nectar)
