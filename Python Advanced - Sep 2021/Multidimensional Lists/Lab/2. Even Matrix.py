e04a331af90054025f061c47750eca7d470fc18e
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-25 11:15:49 +0200

    Added Lab/2. Even Matrix.py

---
+++ Python Advanced - Sep 2021/Multidimensional Lists/Lab/2. Even Matrix.py
@@ -0,0 +1,19 @@
+# Add to the matrix only the even nums and print the matrix
+n = int(input())
+
+def read_matrix():
+    matrix = []
+    for _ in range(n):
+        row = [int(x) for x in input().split(", ")]
+        matrix.append(row)
+    return matrix
+
+
+matrix = read_matrix()
+even_matrix = []
+
+for row in matrix:
+    even = [x for x in row if x % 2 == 0]
+    even_matrix.append(even)
+
+print(even_matrix)
