67628b736f4240f9443a9cbcb15a6c83a0779b8c
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-09-27 13:23:35 +0200

    Added Exercise/7. Snake Moves.py

---
+++ Python Advanced - Sep 2021/Multidimensional Lists/Exercise/7. Snake Moves.py
@@ -0,0 +1,16 @@
+r, c = [int(x) for x in input().split()]
+word = input()
+matrix = []
+index = 0
+
+for row in range(r):
+    matrix.append([None] * c)
+    for col in range(c):
+        if row % 2 == 0:
+            matrix[row][col] = word[index]
+        else:
+            matrix[row][c - 1 - col] = word[index]
+        index = (index + 1) % len(word)
+
+for x in matrix:
+    print(''.join([str(a) for a in x]))
