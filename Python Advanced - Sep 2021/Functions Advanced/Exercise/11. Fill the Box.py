cc99c2d1980fad3f35b001c425e39165966159b7
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-04 00:52:42 +0200

    Added Exercise/11. Fill the Box.py

---
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/11. Fill the Box.py
@@ -0,0 +1,23 @@
+def fill_the_box(height, length, width, *args):
+    box_area = height * length * width
+    failed = False
+    left_cubes = 0
+
+    for el in args:
+        if el == "Finish":
+            break
+
+        if el > box_area and not failed:
+            failed = True
+            left_cubes += el - box_area
+            continue
+        
+        if failed:
+            left_cubes += el
+        else:
+            box_area -= el
+
+    if failed:
+        return f"No more free space! You have {left_cubes} more cubes."
+    else:
+        return f"There is free space in the box. You could put {box_area} more cubes."
