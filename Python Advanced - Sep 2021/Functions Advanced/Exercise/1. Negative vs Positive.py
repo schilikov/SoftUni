62ff58e13736c4b63f7e4a0d193f94dfa2c89937
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-03 21:17:41 +0200

    Added Exercise/1. Negative vs Positive.py

---
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/1. Negative vs Positive.py
@@ -0,0 +1,11 @@
+nums = [int(x) for x in input().split()]
+positive_sum = sum(filter(lambda x: x > 0, nums))
+negative_sum = sum(filter(lambda x: x < 0, nums))
+
+print(negative_sum)
+print(positive_sum)
+
+if abs(negative_sum) > positive_sum:
+    print("The negatives are stronger than the positives")
+else:
+    print("The positives are stronger than the negatives")
