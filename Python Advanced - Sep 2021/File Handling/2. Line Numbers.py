67604e58e9aff2b5b9238b7a76463e50acb4aaac
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-05 16:11:16 +0200

    Added File Handling/2. Line Numbers.py

---
+++ Python Advanced - Sep 2021/File Handling/2. Line Numbers.py
@@ -0,0 +1,15 @@
+symbols = {"-", ",", ".", "!", "?", ":", ";", "_", '"', "'"}
+
+with open("text.txt", "r") as file, open("output.txt", "w") as result:
+    idx = 1
+    for line in file:
+        letters_count = 0
+        punctuation_marks_count = 0
+        for ch in line:
+            if ch in symbols:
+                punctuation_marks_count += 1
+            elif ch.isalpha():
+                letters_count += 1
+        result.write(f"Line {idx}: {line.strip()} ({letters_count})({punctuation_marks_count})")
+        result.write("\n")
+        idx += 1
