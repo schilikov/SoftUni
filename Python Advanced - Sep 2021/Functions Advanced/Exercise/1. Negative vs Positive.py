af3933f1411fc571d4700ee3a3e928b3731c7177
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 13:15:18 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/1. Negative vs Positive.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/1. Negative vs Positive.py
@@ -1,3 +1,4 @@
+# Compare sums of both positive and negative numbers
 nums = [int(x) for x in input().split()]
 positive_sum = sum(filter(lambda x: x > 0, nums))
 negative_sum = sum(filter(lambda x: x < 0, nums))
@@ -9,3 +10,7 @@ if abs(negative_sum) > positive_sum:
     print("The negatives are stronger than the positives")
 else:
     print("The positives are stronger than the negatives")
+
+# Test Inputs
+# 1 2 -3 -4 65 -98 12 57 -84
+# 1 2 3
--- Python Advanced - Sep 2021/Functions Advanced/Exercise/2. Odd or Even.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/2. Odd or Even.py
@@ -1,3 +1,4 @@
+# Print the sum divided by the length of the numbers depending on the give command
 command = input()
 numbers = [int(x) for x in input().split()]
 
@@ -5,3 +6,9 @@ parity = 0 if command == "Even" else 1
 filtered_numbers = sum(filter(lambda x: x % 2 == parity, numbers))
 
 print(filtered_numbers * len(numbers))
+
+# Test Inputs
+# Odd
+# 1 3 5 34 7 9 12 11 13 10
+# Even
+# 1 3 5 7 9 13
