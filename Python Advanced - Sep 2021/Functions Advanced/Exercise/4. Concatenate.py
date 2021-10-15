32818138735a5b0b83c83eacac84bc98a2383a9b
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 13:28:36 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/4. Concatenate.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/4. Concatenate.py
@@ -1,3 +1,4 @@
+# A functions that receives some strings, concatenates them, and returns the result
 def concatenate(*args):
     result = ""
     for x in args:
@@ -6,3 +7,7 @@ def concatenate(*args):
 
 
 concatenate(input())
+
+# Test Codes
+# print(concatenate("Soft", "Uni", "Is", "Great", "!"))
+# print(concatenate("I", " ", "Love", " ", "Python"))
