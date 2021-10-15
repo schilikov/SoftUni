50e7c994f134dd1a5b3f067329f6bbfada4d9db9
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 14:17:29 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/10. Recursive Power.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/10. Recursive Power.py
@@ -1,4 +1,9 @@
+# Using recursion, return the result of number ** power
 def recursive_power(number, power):
     if power == 1:
         return number
     return number * recursive_power(number, power - 1)
+
+# Test Codes
+# print(recursive_power(2, 10))
+# print(recursive_power(10, 100))
