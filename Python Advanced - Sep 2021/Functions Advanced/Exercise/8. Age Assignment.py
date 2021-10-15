a3537189c2e9918e5edfed65f0febb659bcaf30c
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 14:08:06 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/8. Age Assignment.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/8. Age Assignment.py
@@ -1,6 +1,11 @@
+# Assign the given kwargs to the args and print them in a dictionary
 def age_assignment(*args, **kwargs):
     result = {}
     for name in args:
         first_letter = name[0]
         result[name] = kwargs[first_letter]
     return result
+
+# Test Codes
+# print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
--- Python Advanced - Sep 2021/Functions Advanced/Exercise/9. Recursion Palindrome.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/9. Recursion Palindrome.py
@@ -1,6 +1,12 @@
+# Check if the given word is a palindrome or not
 def palindrome(word, index):
     if index == len(word) // 2:
         return f"{word} is a palindrome"
     if word[index] != word[len(word) - 1 - index]:
         return f"{word} is not a palindrome"
     return palindrome(word, index + 1)
+
+# Test Codes
+# print(palindrome("abcba", 0))
+# print(palindrome("peter", 0))
+
