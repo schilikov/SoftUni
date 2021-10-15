b7e6c4adea35b8479cb2ab246d400482b77c79c4
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 13:49:36 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/7. Keyword Arguments Length.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/7. Keyword Arguments Length.py
@@ -1,2 +1,9 @@
+# A function that reads the length of the kwargs
 def kwargs_length(**kwargs):
     return len(kwargs)
+
+# Test Codes
+# dictionary = {'name': 'Peter', 'age': 25}
+# print(kwargs_length(**dictionary))
+# dictionary = {}
+# print(kwargs_length(**dictionary))
