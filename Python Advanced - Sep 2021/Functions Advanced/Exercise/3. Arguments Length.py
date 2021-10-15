05ef1339b3a7af80bcd2eadd1c086887d3a70bb8
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-15 13:25:29 +0200

    Changes/Optimizations/Bug fixes in some tasks

--- Python Advanced - Sep 2021/Functions Advanced/Exercise/3. Arguments Length.py
+++ Python Advanced - Sep 2021/Functions Advanced/Exercise/3. Arguments Length.py
@@ -1,5 +1,11 @@
+# Print the length of the given arguments
 def args_length(*args):
     return len(args)
 
 
 args_length(input().split(", "))
+
+# Test Codes
+# print(args_length(1, 32, 5))
+# print(args_length("john", "peter"))	2
+# print(args_length([1, 2, 3]))	1
