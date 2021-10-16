8988258b545b15f52955ddf64e7fdc15b7aa099d
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-17 01:15:05 +0200

    Added Exam Preparation/2. Darts.py

---
+++ Python Advanced - Sep 2021/Exam Preparation/2. Darts.py
@@ -0,0 +1,66 @@
+import math
+
+
+def is_outside(size, r, c):
+    if r >= size or c >= size or r < 0 or c < 0:
+        return True
+    return False
+
+n = 7
+player_one, player_two = input().split(", ")
+
+matrix = []
+for _ in range(n):
+    matrix.append(input().split())
+
+turn = {
+    1: player_one,
+    0: player_two
+}
+
+points = {
+    player_one: 501,
+    player_two: 501
+}
+
+turns_count = 1
+
+while points[player_one] > 0 and points[player_two] > 0:
+    row, col = [int(el) for el in input()[1:-1].split(", ")]
+
+    if turns_count % 2 == 1:
+        current_player = player_one
+    else:
+        current_player = player_two
+
+    if is_outside(n, row, col):
+        turns_count += 1
+        continue
+    else:
+        current_hit = matrix[row][col]
+        if current_hit.isdigit():
+            points[current_player] -= int(current_hit)
+        if current_hit == "D":
+            hit_sum = \
+                (int(matrix[0][col]) +
+                 int(matrix[row][n - 1]) +
+                 int(matrix[n - 1][col]) +
+                 int(matrix[row][0])) * 2
+            points[current_player] -= hit_sum
+        if current_hit == "T":
+            hit_sum = \
+                (int(matrix[0][col]) +
+                 int(matrix[row][n - 1]) +
+                 int(matrix[n - 1][col]) +
+                 int(matrix[row][0])) * 3
+            points[current_player] -= hit_sum
+        if current_hit == "B":
+            print(f"{current_player} won the game with {math.ceil(turns_count / 2)} throws!")
+            exit(0)
+
+        if points[player_one] <= 0 or points[player_two] <= 0:
+            break
+
+    turns_count += 1
+
+print(f"{turn[turns_count % 2]} won the game with {math.ceil(turns_count / 2)} throws!")
