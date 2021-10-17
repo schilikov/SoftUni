af17a6be6217b0f7b1a6711724f9048b6d81d276
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-17 22:58:01 +0200

    Added December 2020/3. Problem.py

---
+++ Python Advanced - Sep 2021/Exam Preparation/December 2020/3. Problem.py
@@ -0,0 +1,13 @@
+def get_magic_triangle(number):
+    triangle = [[1], [1, 1]]
+    for row in range(2, number):
+        triangle.append([])
+        triangle[-1].append(1)
+        for col in range(1, row):
+            triangle[-1].append(
+                triangle[row - 1][col - 1] +
+                triangle[row - 1][col]
+            )
+        triangle[-1].append(1)
+
+    return triangle
