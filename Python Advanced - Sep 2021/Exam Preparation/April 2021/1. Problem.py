0109412319324397312f41513d25709605c42a2a
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-17 14:58:07 +0200

    Added April 2021/1. Problem.py

---
+++ Python Advanced - Sep 2021/Exam Preparation/April 2021/1. Problem.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+pizza_orders = deque([int(x) for x in input().split(", ")])
+making_capacity = [int(x) for x in input().split(", ")]
+
+total_pizzas_made = 0
+orders = len(pizza_orders)
+
+while True:
+    if len(pizza_orders) == 0 or len(making_capacity) == 0 or orders == 0:
+        break
+
+    current_order = pizza_orders.popleft()
+    current_employee = making_capacity.pop()
+
+    if current_order > 10 or current_order <= 0:
+        making_capacity.append(current_employee)
+        continue
+
+    if current_order <= current_employee:
+        orders -= 1
+        total_pizzas_made += current_order
+    else:
+        pizza_orders.appendleft(current_order - current_employee)
+        total_pizzas_made += current_employee
+
+if len(pizza_orders) == 0:
+    print("All orders are successfully completed!")
+    print(f"Total pizzas made: {total_pizzas_made}")
+    print(f"Employees: {', '.join([str(x) for x in making_capacity])}")
+
+if len(making_capacity) == 0:
+    print("Not all orders are completed.")
+    print(f"Orders left: {', '.join([str(x) for x in pizza_orders])}")
