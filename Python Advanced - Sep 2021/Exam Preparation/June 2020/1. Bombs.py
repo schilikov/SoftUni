4ff2a4e7f97a049f2f3d9e862dec95ed9ff2108e
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-19 15:44:04 +0200

    Added June 2020/1. Bombs.py

---
+++ Python Advanced - Sep 2021/Exam Preparation/June 2020/1. Bombs.py
@@ -0,0 +1,59 @@
+from collections import deque
+
+bomb_effects = deque([int(x) for x in input().split(", ")])
+bomb_casings = [int(x) for x in input().split(", ")]
+
+bombs_count = {
+    "Datura Bombs": 0,
+    "Cherry Bombs": 0,
+    "Smoke Decoy Bombs": 0
+}
+
+datura, cherry, smoke_decoy = 40, 60, 120
+
+success = False
+while bomb_effects and bomb_casings:
+    current_effect = bomb_effects.popleft()
+    current_casing = bomb_casings.pop()
+    sum_of_bomb = current_effect + current_casing
+
+    if sum_of_bomb == datura:
+        bombs_count["Datura Bombs"] += 1
+    elif sum_of_bomb == cherry:
+        bombs_count["Cherry Bombs"] += 1
+    elif sum_of_bomb == smoke_decoy:
+        bombs_count["Smoke Decoy Bombs"] += 1
+    else:
+        bomb_effects.appendleft(current_effect)
+        bomb_casings.append(current_casing - 5)
+
+    num = 0
+    for x in bombs_count.keys():
+        if bombs_count[x] >= 3:
+            num += 1
+
+    if num == 3:
+        success = True
+        break
+    else:
+        num = 0
+
+if success:
+    print("Bene! You have successfully filled the bomb pouch!")
+else:
+    print("You don't have enough materials to fill the bomb pouch.")
+
+if bomb_effects:
+    print(f"Bomb Effects: {', '.join([str(x) for x in bomb_effects])}")
+else:
+    print("Bomb Effects: empty")
+
+if bomb_casings:
+    print(f"Bomb Casings: {', '.join([str(x) for x in bomb_casings])}")
+else:
+    print("Bomb Casings: empty")
+
+sorted_dict = sorted(bombs_count.items(), key=lambda x: x[0])
+
+for key, value in sorted_dict:
+    print(f"{key}: {value}")
