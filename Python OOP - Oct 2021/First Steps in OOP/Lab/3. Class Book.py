35398192035174f2c867696d13569f337a3146fb
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-26 22:07:41 +0200

    Added Lab/4. Car.py

--- Python OOP - Oct 2021/First Steps in OOP/Lab/3. Class Book.py
+++ Python OOP - Oct 2021/First Steps in OOP/Lab/3. Class Book.py
@@ -1,8 +1,8 @@
 class Book:
-    def __init__(self, name, author, pages):
-        self.name = str(name)
-        self.author = str(author)
-        self.pages = int(pages)
+    def __init__(self, name: str, author: str, pages: int):
+        self.name = name
+        self.author = author
+        self.pages = pages
 
 
 book = Book("My Book", "Me", 200)
