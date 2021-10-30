0e1eb05665cffd77539f2b7e98608f04d2e47529
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-10-30 20:34:54 +0200

    Small change

--- Python OOP - Oct 2021/Classes and Objects/Exercise/8. Library/project/user.py
+++ Python OOP - Oct 2021/Classes and Objects/Exercise/8. Library/project/user.py
@@ -1,18 +1,15 @@
-from project.library import Library
-
-
 class User:
     def __init__(self, user_id: int, username):
         self.user_id = user_id
         self.username = username
         self.books = []
 
-    def get_book(self, author: str, book_name: str, days_to_return: int, library: Library):
+    def get_book(self, author, book_name, days_to_return, library):
         for _, user_rented_books in library.rented_books.items():
             for rented_book_name, rented_book_days_to_return in user_rented_books.items():
                 if rented_book_name == book_name:
-                    return f"The book '{book_name}' is already rented and " \
-                           f"will be available in {rented_book_days_to_return} days!"
+                    return f'The book "{book_name}" is already rented and ' \
+                           f'will be available in {rented_book_days_to_return} days!'
 
         if author not in library.books_available:
             return
@@ -29,13 +26,13 @@ class User:
         self.books.append(book_name)
         return f"{book_name} successfully rented for the next {days_to_return} days!"
 
-    def return_book(self, author:str, book_name:str, library: Library):
+    def return_book(self, author, book_name, library):
         if book_name not in self.books:
             return f"{self.username} doesn't have this book in his/her records!"
 
         self.books.remove(book_name)
-        library.books_available[author].append(book_name)
         library.rented_books[self.username].pop(book_name)
+        library.books_available[author].append(book_name)
 
     def info(self):
         return ', '.join(sorted(self.books))
