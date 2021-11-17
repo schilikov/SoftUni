f36cf851092840c753bbc9607803fa8aa39cd396
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-11-17 12:02:57 +0100

    Added new task

--- Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/emails.py
+++ Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/emails.py
@@ -1,8 +1,21 @@
-from abc import ABCMeta, abstractmethod
+from abc import abstractmethod, ABC
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
 
+class IContent(ABC):
+    def __init__(self, content_type):
+        self.content_type = content_type
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class Content(IContent):
+    def format(self):
+        return '\n'.join(['<myML>', self.content_type, '</myML>'])
+
+
+class IEmail(ABC):
     @abstractmethod
     def set_sender(self, sender):
         pass
@@ -15,11 +28,10 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
-class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+class Email(IEmail):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -37,21 +49,17 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content
 
     def __repr__(self):
-
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content.format())
 
 
-email = Email('IM', 'MyML')
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+content = Content('Hello, there!')
+email.set_content(content)
 print(email)
-
