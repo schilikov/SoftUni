5a3aa774c900c560ca65f52df62db494c8c3aa39
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-11-17 11:30:07 +0100

    Added new task

--- Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/shapes.py
+++ Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/shapes.py
@@ -1,13 +1,34 @@
-class Rectangle:
+from abc import abstractmethod, ABC
 
+
+class Shape(ABC):
+    @abstractmethod
+    def calc_area(self):
+        pass
+
+
+class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
+    def calc_area(self):
+        area = self.width * self.height
+        return area
+
+
+class Triangle(Shape):
+    def __init__(self, side, height):
+        self.side = side
+        self.height = height
+
+    def calc_area(self):
+        area = (self.side * self.height) / 2
+        return area
+
+
 class AreaCalculator:
-
     def __init__(self, shapes):
-
         assert isinstance(shapes, list), "`shapes` should be of type `list`."
         self.shapes = shapes
 
@@ -15,11 +36,12 @@ class AreaCalculator:
     def total_area(self):
         total = 0
         for shape in self.shapes:
-            total += shape.width * shape.height
+            total += shape.calc_area()
 
         return total
 
 
-shapes = [Rectangle(2, 3), Rectangle(1, 6)]
+shapes = [Rectangle(1, 6), Triangle(2, 3)]
 calculator = AreaCalculator(shapes)
+
 print("The total area is: ", calculator.total_area)
