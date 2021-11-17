fb382db6df179fd570f4bf3c99c5959efecf40c7
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-11-17 11:17:07 +0100

    Added new task

--- Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/prisoner.py
+++ Python OOP - Oct 2021/SOLID/Exercise/exercise-resources/exercise-resources/prisoner.py
@@ -1,9 +1,15 @@
-import copy
+from abc import ABC, abstractmethod
 
-class Person:
 
+class Person(ABC):
+    @abstractmethod
     def __init__(self, position):
         self.position = position
+
+
+class FreePerson(Person):
+    def __init__(self, position):
+        super().__init__(position)
 
     def walk_north(self, dist):
         self.position[1] += dist
@@ -13,12 +19,10 @@ class Person:
 
 
 class Prisoner(Person):
-    PRISON_LOCATION = [3, 3]
+    PRISON_LOCATION = (3, 3)
 
     def __init__(self):
-        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
-        self.is_free = False
-
+        super().__init__(self.PRISON_LOCATION)
 
 
 prisoner = Prisoner()
