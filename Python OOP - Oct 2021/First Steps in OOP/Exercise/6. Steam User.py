860a134630f4f586b3a62df4114799022f928864
Simeon Chilikov <simeon.chilikov@gmail.com>
2021-11-28 19:43:25 +0100

    Small changes

--- Python OOP - Oct 2021/First Steps in OOP/Exercise/6. Steam User.py
+++ Python OOP - Oct 2021/First Steps in OOP/Exercise/6. Steam User.py
@@ -5,18 +5,18 @@ class SteamUser:
         self.played_hours = 0
 
     def play(self, game, hours):
-        if game in self.games:
-            self.played_hours += hours
-            return f"{self.username} is playing {game}"
-        else:
+        if game not in self.games:
             return f"{game} is not in library"
 
+        self.played_hours += hours
+        return f"{self.username} is playing {game}"
+
     def buy_game(self, game):
-        if game not in self.games:
-            self.games.append(game)
-            return f"{self.username} bought {game}"
-        else:
+        if game in self.games:
             return f"{game} is already in your library"
+
+        self.games.append(game)
+        return f"{self.username} bought {game}"
 
     def status(self):
         return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
