5d7c11a3ed33ff9e158877a7b6617308578ae9ff
Simeon Chilikov <simeon.chilikov@gmail.com>
2022-03-18 12:12:43 +0100

    Workshop Project

--- employees_app/employees_app/employees/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from Employees_App.employees.views import department_details, list_departments
-
-urlpatterns = (
-    path('list/', list_departments, name='list departments'),
-    path('<int:id>/', department_details, name='department details'),
-)
